05744b576295085a940c91574bb6c8dc9d725fa0
aiiaaan <63637035+aiiaaan@users.noreply.github.com>
2020-04-23 07:26:01 +0800

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -7,6 +7,6 @@ cd ~
 mkdir Haise
 cd Haise
 rm -rf *
-wget https://github.com/aiiaaan/panel-theme/blob/master/view.tgz && tar zxvf view.tgz
-mv view /home/panel/html
+wget https://github.com/aiiaaan/panel-theme/blob/master/view.zip
+unzip view.tgz -d /home/panel/html
 echo "Enjoy using Premium Template -Haise"
