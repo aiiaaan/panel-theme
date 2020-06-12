bbe138f6402cb4fc9282acd55a0c01ff935932f5
aiiaaan <63637035+aiiaaan@users.noreply.github.com>
2020-06-12 21:05:22 +0800

    Updated

--- theme.sh
+++ theme.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Removing Old Template"
 rm -rf /home/panel/html/view
+rm -rf /home/panel/html/controller
 rm -rf /home/panel/html/tmp/*
 echo "Installing New Template"
 cd ~
@@ -8,5 +9,8 @@ mkdir Haise
 cd Haise
 rm -rf *
 wget https://github.com/aiiaaan/panel-theme/blob/master/view.zip
-unzip view.tgz -d /home/panel/html
+unzip view.zip -d /home/panel/html
+
+wget https://github.com/aiiaaan/panel-theme/blob/master/controller.zip
+unzip controller.zip -d /home/panel/html
 echo "Enjoy using Premium Template -Haise"
